7491f70259bc412ea5cd731fc0614c1fa8b83db3
Lucas Pereira <lucasp28.contato@gmail.com>
2019-04-24 19:25:06 -0300

    ranking ordenado pela reputation

--- app/Http/Controllers/Levelup/RankingController.php
+++ app/Http/Controllers/Levelup/RankingController.php
@@ -12,16 +12,50 @@ namespace App\Http\Controllers\Levelup;
 use App\Entities\Auth\User;
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
 
 class RankingController extends Controller
 {
     use ApiResponse;
 
-    public function index(){
-        $data = User::orderBy('level','DESC')
-            ->orderBy('current_exp','DESC')
-            ->select('discord_id','current_exp','level')
-            ->paginate(10);
+    /**
+     * @OA\Get(
+     *     path="/ranking",
+     *     summary="Lista o raking de usuários",
+     *     operationId="GetRanking",
+     *     tags={"ranking"},
+     *     @OA\Parameter(
+     *         name="reputation",
+     *         in="query",
+     *         description="Ordenar por reputation (0, 1)",
+     *         required=false,
+     *         @OA\Schema(
+     *           type="integer",
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="...",
+     *     )
+     * )
+     */
+    public function index(Request $request)
+    {
+        $this->validate($request, [
+            'reputation' => 'boolean'
+        ]);
+
+        if ($request->input('reputation')) {
+            $data = User::orderBy('reputation', 'DESC')
+                ->select('discord_id', 'reputation', 'current_exp', 'level')
+                ->paginate(10);
+        } else {
+            $data = User::orderBy('level', 'DESC')
+                ->orderBy('current_exp', 'DESC')
+                ->select('discord_id', 'current_exp', 'level', 'reputation')
+                ->paginate(10);
+        }
+
         return $this->success($data);
     }
 }
