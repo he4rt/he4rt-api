bdb9d7e6af484b2e445882d2fe3eefeedfdb5318
Alexandre de Souza <alexandre@aledsz.com.br>
2020-06-08 19:00:16 -0300

    Add JSON view

---
+++ lib/he4rt/views/v1/json_view.ex
@@ -0,0 +1,14 @@
+defmodule He4rt.Views.V1.JsonView do
+  @moduledoc """
+  View dedicated to handle responses as JSON
+  """
+  alias He4rt.Utils.JSON
+
+  @doc """
+  Based on response type, return it as JSON to API
+  """
+  @spec handle_response(map | struct | list | any) :: map | list
+  def handle_response(response) when is_list(response), do: JSON.serialize(response)
+  def handle_response(response) when is_map(response), do: JSON.serialize(response)
+  def handle_response(response), do: JSON.serialize(%{"response" => response})
+end
