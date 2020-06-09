402d08c98fd095af3a0e52663f0cc6477ba2099b
Alexandre de Souza <alexandre@aledsz.com.br>
2020-06-08 23:47:31 -0300

    Add module for LevelUps

---
+++ lib/he4rt/modules/levelups.ex
@@ -0,0 +1,95 @@
+defmodule He4rt.Modules.LevelUps do
+  @moduledoc """
+  The Level Ups context.
+
+  Handle level ups from Database
+  """
+  import Ecto.Query
+
+  alias He4rt.Repo
+  alias He4rt.Schemas.LevelUp
+
+  @preload []
+
+  @doc """
+  Retrieve one level up by ID
+  """
+  @spec get(level_up_id :: pos_integer()) :: {:ok, LevelUp.t} | {:error, term}
+  def get(level_up_id) when is_integer(level_up_id) do
+    from(
+      m in LevelUp,
+      where: m.id == ^level_up_id,
+      preload: ^@preload
+    )
+    |> Repo.one()
+    |> case do
+      nil ->
+        {:error, :not_found}
+
+      level_up ->
+        {:ok, level_up}
+    end
+  end
+
+  @doc """
+  Retrieve one level up by name
+  """
+  @spec get_from_name(name :: String.t) :: {:ok, LevelUp.t} | {:error, term}
+  def get_from_name(name) when is_binary(name) do
+    from(
+      m in LevelUp,
+      where: m.name == ^name,
+      preload: ^@preload
+    )
+    |> Repo.one()
+    |> case do
+      nil ->
+        {:error, :not_found}
+
+      level_up ->
+        {:ok, level_up}
+    end
+  end
+
+  @doc """
+  Retrieve all level ups
+  """
+  @spec retrieve_all() :: {:ok, list(LevelUp.t)} | {:error, term}
+  def retrieve_all() do
+    from(
+      m in LevelUp,
+      preload: ^@preload,
+      order_by: [desc: m.created_at]
+    )
+    |> Repo.all()
+    |> case do
+      [] ->
+        {:error, :not_found}
+
+      level_ups ->
+        {:ok, level_ups}
+    end
+  end
+
+  @doc """
+  Create new level up
+  """
+  @spec create(attrs :: map()) :: {:ok, LevelUp.t} | {:error, term}
+  def create(attrs) when is_map(attrs) do
+    %LevelUp{}
+    |> LevelUp.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Update existent level up
+  """
+  @spec update(attrs :: map()) :: {:ok, LevelUp.t} | {:error, term}
+  def update(%{"level_up_id" => level_up_id} = attrs) when is_integer(level_up_id) and is_map(attrs) do
+    with {:ok, level_up} <- get(level_up_id) do
+      level_up
+      |> LevelUp.changeset(attrs)
+      |> Repo.update()
+    end
+  end
+end
