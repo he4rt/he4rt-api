7d6e6b5edca5ac257b236678e248cd3f30bd118f
Alexandre de Souza <alexandre@aledsz.com.br>
2020-06-08 21:24:29 -0300

    Add configuration for MySQL Repo

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,11 @@ config :logger, :console,
   format: "time=$date $time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# General application configuration
+config :he4rt,
+  ecto_repos: [He4rt.Repo],
+  generators: [integer: true]
+
 # Configures Redis
 config :he4rt, He4rt.Services.Redis,
   url: {:system, "REDIS_URL"}
--- config/test.exs
+++ config/test.exs
@@ -7,6 +7,11 @@ config :logger, level: :info
 config :he4rt, He4rt.Endpoint,
   port: 4000
 
+# Configure your database
+config :he4rt, He4rt.Repo,
+  timeout: 99_999_999,
+  pool: Ecto.Adapters.SQL.Sandbox
+
 # Configure test watch
 config :mix_test_watch,
   clear: true
