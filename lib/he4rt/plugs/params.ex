906164177df7ba473d5c2f20c6006d95a66ad95b
Alexandre de Souza <alexandre@aledsz.com.br>
2020-06-10 02:01:08 -0300

    Improve paramaters plug to convert IDs* to integer
    * except reserved fields

--- lib/he4rt/plugs/params.ex
+++ lib/he4rt/plugs/params.ex
@@ -4,12 +4,18 @@ defmodule He4rt.Plugs.Params do
   """
   import Plug.Conn
 
-  @spec init(List.t()) :: List.t()
+  @reserved_keys ~w(
+    discord_id
+    admin_id
+    victim_id
+  )
+
+  @spec init(keyword()) :: keyword()
   def init(opts) do
     opts
   end
 
-  @spec call(Plug.Conn.t(), List.t()) :: Plug.Conn.t()
+  @spec call(Plug.Conn.t, keyword()) :: Plug.Conn.t
   def call(conn, _opts) do
     conn_params = if Map.has_key?(conn.assigns, :params), do: conn.assigns.params, else: %{}
 
@@ -22,13 +28,37 @@ defmodule He4rt.Plugs.Params do
           params
       end
       |> Map.merge(conn_params)
+      |> case do
+        params ->
+          params
+          |> Enum.map(fn
+            {key, value} when key in @reserved_keys and is_binary(value) ->
+              {key, value}
+
+            {"id" = key, value} when is_binary(value) ->
+              {key, value |> String.to_integer()}
+
+            {key, value} when is_binary(value) ->
+              ~r/_id/
+              |> Regex.match?(key)
+              |> if do
+                {key, value |> String.to_integer()}
+              else
+                {key, value}
+              end
+
+            {key, value} ->
+              {key, value}
+          end)
+          |> Enum.into(%{})
+      end
 
     conn
     |> put_resp_content_type("application/json")
     |> assign(:params, params)
   end
 
-  @spec get_params(Plug.Conn.t()) :: Map.t()
+  @spec get_params(Plug.Conn.t) :: map()
   def get_params(%Plug.Conn{assigns: %{params: params}, path_params: %{"glob" => _glob} = path_params}) do
     params
     |> Map.merge(path_params)
