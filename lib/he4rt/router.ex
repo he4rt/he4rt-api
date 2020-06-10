ff26b49bf22c21fb75cc8445f37984af502e1bde
Alexandre de Souza <alexandre@aledsz.com.br>
2020-06-10 01:58:58 -0300

    Add sub routers

--- lib/he4rt/router.ex
+++ lib/he4rt/router.ex
@@ -6,6 +6,24 @@ defmodule He4rt.Router do
   plug :match
   plug :dispatch
 
+  forward "/categories",
+    to: He4rt.Routes.CategoriesRouter
+
+  forward "/products",
+    to: He4rt.Routes.ProductsRouter
+
+  forward "/coupons",
+    to: He4rt.Routes.CouponsRouter
+
+  forward "/bans",
+    to: He4rt.Routes.BansRouter
+
+  forward "/languages",
+    to: He4rt.Routes.LanguagesRouter
+
+  forward "/health",
+    to: He4rt.Routes.HealthRouter
+
   match _ do
     conn
     |> route_not_found()
