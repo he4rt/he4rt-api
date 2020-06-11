2596d8692337ff239f79878a1634ad212f3107b7
Alexandre de Souza <alexandre@aledsz.com.br>
2020-06-10 23:01:39 -0300

    Add function to use coupon for user

--- lib/he4rt/controllers/coupons_controller.ex
+++ lib/he4rt/controllers/coupons_controller.ex
@@ -5,7 +5,7 @@ defmodule He4rt.Controllers.CouponsController do
   use Plug.Builder
   import He4rt.Views.{JsonView, ErrorView}
 
-  alias He4rt.Modules.Coupons
+  alias He4rt.Modules.{Coupons, Users}
 
   @doc """
   Create new coupon 
@@ -15,6 +15,32 @@ defmodule He4rt.Controllers.CouponsController do
     case Coupons.create(params) do
       {:ok, coupon} ->
         conn |> send_resp(201, coupon |> handle_response())
+
+      {:error, reason} ->
+        conn |> send_error({:error, reason})
+    end
+  end
+
+  @doc """
+  Update new coupon to use with an user
+  """
+  @spec used(Plug.Conn.t, map()) :: Plug.Conn.t
+  def used(%Plug.Conn{} = conn, %{"discord_id" => discord_id, "coupon" => coupon}) do
+    case Users.get_from_discord_id(discord_id) do
+      {:ok, user} ->
+        case Coupons.get_from_name(coupon) do
+          {:ok, coupon} ->
+            case Coupons.update(%{"coupon_id" => coupon.id, "used" => true, "user_id" => user.id}) do
+              {:ok, coupon} ->
+                conn |> send_resp(200, coupon |> handle_response())
+
+              {:error, reason} ->
+                conn |> send_error({:error, reason})
+            end
+
+          {:error, reason} ->
+            conn |> send_error({:error, reason})
+        end
 
       {:error, reason} ->
         conn |> send_error({:error, reason})
