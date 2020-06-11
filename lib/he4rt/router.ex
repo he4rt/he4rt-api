b166f7221835ebcf939d83051ae2149f3410fc7c
Alexandre de Souza <alexandre@aledsz.com.br>
2020-06-10 22:57:50 -0300

    Add new routers

--- lib/he4rt/router.ex
+++ lib/he4rt/router.ex
@@ -15,11 +15,29 @@ defmodule He4rt.Router do
   forward "/coupons",
     to: He4rt.Routes.CouponsRouter
 
+  forward "/users/:discord_id/coupon",
+    to: He4rt.Routes.UserCouponsRouter
+
   forward "/bans",
     to: He4rt.Routes.BansRouter
 
   forward "/languages",
     to: He4rt.Routes.LanguagesRouter
+
+  forward "/users",
+    to: He4rt.Routes.UsersRouter
+
+  forward "/ranking",
+    to: He4rt.Routes.RankingRouter
+
+  forward "/store",
+    to: He4rt.Routes.StoreRouter
+
+  forward "/tips/english/",
+    to: He4rt.Routes.EnglishTipsRouter
+
+  forward "/tips/development/",
+    to: He4rt.Routes.DevelopmentTipsRouter
 
   forward "/health",
     to: He4rt.Routes.HealthRouter
